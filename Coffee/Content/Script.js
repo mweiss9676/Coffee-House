d245f7594ba6049a20e8d3c479e56820e78d395d
Michael Weiss <mweis9676@gmail.com>
2018-03-24 12:50:17 -0400

    first commit, entity framework added, basic css for index complete, carousel working

---
+++ Coffee/Content/Script.js
@@ -0,0 +1,11 @@
+﻿//$(document).ready(() => {
+//    let leftImages = Array.from(document.querySelectorAll("#left-main > img"));
+//    setInterval(() => {
+//        //$("#cs-1").animate({ top: '-100%' }),
+//        //$("#cs-1").animate({ top: '100%' });
+//        let temp = leftImages.shift();
+//        $(temp).animate({ top: '-100%' });
+//        $(temp).animate({ top: '100%' });
+//        leftImages.push(temp);
+//    }, 4000);
+//});
---
+++ Coffee/Models/Bean.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coffee.Models
+{
+    public class Bean
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string Farm { get; set; }
+        public string Variety { get; set; }
+        public int Altitude { get; set; }
+        public string Roast { get; set; }
+        public string Flavor { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+
+    }
+}
