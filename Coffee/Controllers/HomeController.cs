c92403a790ad9485184d24d001e6fd98694d83f6
Michael Weiss <mweis9676@gmail.com>
2018-03-24 16:15:53 -0400

    entity framework added and layout revised. May need to come back to this one after deleting entity to refresh it

--- Coffee/Controllers/HomeController.cs
+++ Coffee/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace Coffee.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
---
+++ Coffee/Data/CoffeeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Coffee.Data
+{
+    public class CoffeeContext : DbContext
+    {
+        public CoffeeContext(): base()
+        {
+
+        }
+
+        public DbSet<Bean> Beans { get; set; }
+
+    }
+}
