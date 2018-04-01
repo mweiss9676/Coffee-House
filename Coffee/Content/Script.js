9b340d44a5f1272c0a97050f899f08464f807621
Michael Weiss <mweis9676@gmail.com>
2018-04-01 12:35:09 -0400

    started making it responsive

--- Coffee/Content/Script.js
+++ Coffee/Content/Script.js
@@ -4,21 +4,30 @@ var count = 0;
 
 $(window).on("load", function () {
 
-    $('.bean-info-box').hover(function () {
-        $(this).addClass('transition');
-        $(this).css({
-            boxShadow: '0 28px 15px -15px rgba(50, 20, 20, .4)'
+    if ($(window).width < 451) {
+        shrinkHeader();
+    }
+
+
+    if ($(window).width > 450) {
+        $('.bean-info-box').hover(function () {
+            $(this).addClass('transition');
+            $(this).css({
+                boxShadow: '0 28px 15px -15px rgba(50, 20, 20, .4)'
+            });
+        }, function () {
+            $(this).removeClass('transition');
+            $(this).css({
+                boxShadow: '0 5px 10px -5px rgba(50, 20, 20, .8)'
+            });
         });
-    }, function () {
-        $(this).removeClass('transition');
-        $(this).css({
-            boxShadow: '0 5px 10px -5px rgba(50, 20, 20, .8)'
-        });
-        });
+    }
+
     $('.bean-image').click(function () {
         $(this).siblings('.bean-description').toggleClass('bean-description-expand');
         $(this).siblings().children('.bean-about').toggleClass('hid');
     });
+    
 
 
     $('.add-to-cart').click(function () {
@@ -67,26 +76,34 @@ function UpdateCount() {
     count = ids.length;
 }
 
+function shrinkHeader() {
+    $('#brand').text("CCC.");
+    $('header').css({
+        height: '100px',
+        top: '0',
+        left: '0'
+    });
+    $('#brand').addClass('title-flip');
+}
+
+function expandHeader() {
+    $('#brand').text("CLEVELAND COFFEE CO.");
+    $('header').css({
+        height: 'inherit',
+        top: '0',
+        left: '0'
+    });
+    $('#brand').removeClass('title-flip');
+}
+
 $(window).scroll(function () {
     let scrollPixels = parseInt($(window).scrollTop());
     let top = 250;
 
     if (scrollPixels > 150) {
-        $('#brand').text("CCC.");
-        $('header').css({
-            height: '100px',
-            top: '0',
-            left: '0'
-        });
-        $('#brand').addClass('title-flip');
+        shrinkHeader();
     } else {
-        $('#brand').text("CLEVELAND COFFEE CO.");
-        $('header').css({
-            height: 'inherit',
-            top: '0',
-            left: '0'
-        });
-        $('#brand').removeClass('title-flip');
+        expandHeader();
     }
 });
 
